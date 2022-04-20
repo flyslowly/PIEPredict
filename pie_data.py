2041ce7808a71577c842be370822030b7d2877af
FlySlowly <jiangnan.flyslowly@gmail.com>
2022-04-20 15:02:20 +0800

    Fixed type error

--- pie_data.py
+++ pie_data.py
@@ -1001,9 +1001,12 @@ class PIE(object):
                                     for i in frame_ids][::seq_stride])
                     for i in range(len(frame_ids)):
                         center_point = self._get_center(boxes[i])
-                        row = ['{:.1f}'.format(frame_ids[i] * 10), '{:.1f}'.format(int(pid.split('_')[2]))]
+                        row = []
+                        frame_id = '{:.1f}'.format(frame_ids[i] * 10)
+                        pedestrian_id = '{:.1f}'.format(int(pid.split('_')[2]))
+                        row = [frame_ids, pedestrian_id]
                         row = row.append(center_point)
-                        print(row)
+                        print(type(row))
                         csv_writer.writerow(row)
                 
                 f.close()
