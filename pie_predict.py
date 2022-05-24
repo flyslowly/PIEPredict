a07ff95009ff5e11873c40969859dd257960bf9f
FlySlowly <jiangnan.flyslowly@gmail.com>
2022-05-24 23:42:32 +0200

    Try shape

--- pie_predict.py
+++ pie_predict.py
@@ -376,7 +376,7 @@ class PIEPredict(object):
         print("Try print data input and output:")
         print("###################")
         print("xxxxxxxxxxxxxxxxxxxxxxxxxx")
-        print(tf.compat.v1.size(train_data[0]))
+        print(train_data[0].shape)
         print("yyyyyyyyyyyyyyyyyyyyyy")
         print(tf.compat.v1.size(train_data[1]))
 
